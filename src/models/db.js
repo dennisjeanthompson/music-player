444d8bc009b38b4543e1b7430be9a07e6650d792
dennis <146879816+dennisjeanthompson@users.noreply.github.com>
2024-12-07 00:22:25 +0800

    Update db.js

--- src/models/db.js
+++ src/models/db.js
@@ -1,4 +1,3 @@
-
 import sqlite3 from 'sqlite3';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -27,26 +26,6 @@ export function getAllSongs() {
     db.all('SELECT * FROM songs ORDER BY date_created DESC', (err, rows) => {
       if (err) reject(err);
       resolve(rows);
-
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
-
-const dbPath = path.join(__dirname, '../../data/music.db');
-const db = new sqlite3.Database(dbPath);
-
-function init() {
-  return new Promise((resolve, reject) => {
-    db.run(`CREATE TABLE IF NOT EXISTS tracks (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name VARCHAR(255) NOT NULL,
-      artist VARCHAR(255),
-      description TEXT,
-      file_path TEXT,
-      date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )`, (err) => {
-      if (err) reject(err);
-      else resolve();
-
     });
   });
 }
@@ -112,8 +91,3 @@ export function searchSongs(query) {
 }
 
 export default db;
-
-module.exports = {
-  db,
-  init
-};
